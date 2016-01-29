453ef50aac7360705749985ce8b1b2c2f05c7933
Gumin Egor <gumin.egor@gmail.com>
2016-01-29 03:32:41 +0300

    Added resume download

--- app/src/main/java/com/guminegor/example/downloadingpicture/StackOverflowActivity.java
+++ app/src/main/java/com/guminegor/example/downloadingpicture/StackOverflowActivity.java
@@ -1,14 +1,5 @@
 package com.guminegor.example.downloadingpicture;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.net.URL;
-
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -16,13 +7,32 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class StackOverflowActivity extends Activity {
 
     //
     private ImageView mImageView;
+    private ProgressBar imageProgressBar;
+    private int wasStoppedOnFile = -1;
 
     /** Called when the activity is first created. */
     @Override
@@ -30,6 +40,9 @@ public class StackOverflowActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        imageProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        //imageProgressBar.show
 
         //Find the reference to the ImageView
         mImageView = (ImageView) findViewById(R.id.test_image);
@@ -41,7 +54,19 @@ public class StackOverflowActivity extends Activity {
         //http://i.imgur.com/hK8u0GU.jpg
         //http://i.imgur.com/0pAdPiM.jpg
 
-        new DownloadImage().execute("http://guminegor.github.io/DownloadingPicture/images/image0.bmp");
+        //new DownloadImage().execute("http://guminegor.github.io/DownloadingPicture/images/image0.bmp");
+
+        new DownloadImageTaskSafe().execute("http://guminegor.github.io/DownloadingPicture/images/image0.bmp");
+
+        int a = 0;
+        int b = 1;
+    }
+
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        new DownloadImageTaskSafe().execute("http://guminegor.github.io/DownloadingPicture/images/image0.bmp");
     }
 
 
@@ -60,6 +85,10 @@ public class StackOverflowActivity extends Activity {
         protected Drawable doInBackground(String... arg0) {
             // This is done in a background thread
             return downloadImage(arg0[0]);
+        }
+
+        protected void onProgressUpdate(String... progress) {
+            imageProgressBar.setProgress(Integer.parseInt(progress[0]));
         }
 
         /**
@@ -89,6 +118,8 @@ public class StackOverflowActivity extends Activity {
                 url = new URL(_url);
                 in = url.openStream();
 
+
+
             /*
              * THIS IS NOT NEEDED
              *
@@ -109,6 +140,8 @@ public class StackOverflowActivity extends Activity {
                 // Read the inputstream
                 buf = new BufferedInputStream(in);
 
+
+
                 // Convert the BufferedInputStream to a Bitmap
                 Bitmap bMap = BitmapFactory.decodeStream(buf);
                 if (in != null) {
@@ -127,6 +160,147 @@ public class StackOverflowActivity extends Activity {
             return null;
         }
 
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, String, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            imageProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            Bitmap bitmap = null;
+            try {
+                int increment;
+                byte[] data;
+                InputStream in = null;
+                int response;
+                URL url = new URL(params[0]);
+                URLConnection conn = url.openConnection();
+                if (!(conn instanceof HttpURLConnection))
+                    throw new IOException("Not an HTTP connection");
+                try {
+                    HttpURLConnection httpConn = (HttpURLConnection) conn;
+
+                    httpConn.setInstanceFollowRedirects(true);
+                    httpConn.setRequestMethod("GET");
+                    httpConn.connect();
+
+                    response = httpConn.getResponseCode();
+                    if (response == HttpURLConnection.HTTP_OK) {
+                        in = httpConn.getInputStream();
+                    }
+                    int length = httpConn.getContentLength();
+
+                    data = new byte[length];
+                    increment = length / 100;
+                    ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+                    int count = -1;
+                    int progress = 0;
+
+                    while ((count = in.read(data, 0, increment)) != -1) {
+                        progress += count;
+                        publishProgress("" + (int) ((progress * 100) / length));
+                        outStream.write(data, 0, count);
+                    }
+                    bitmap = BitmapFactory.decodeByteArray(
+                            outStream.toByteArray(), 0, data.length);
+                    in.close();
+                    outStream.close();
+
+                } catch (Exception ex) {
+                    Log.d("Networking", ex.getLocalizedMessage());
+                    wasStoppedOnFile = 0;
+                    throw new IOException("Error connecting");
+                }
+
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
+            }
+            return bitmap;
+        }
+
+        protected void onProgressUpdate(String... progress) {
+            imageProgressBar.setProgress(Integer.parseInt(progress[0]));
+        }
+
+        protected void onPostExecute(Bitmap bm) {
+
+            imageProgressBar.setVisibility(View.GONE);
+
+            if (bm != null) {
+                mImageView.setImageBitmap(bm);
+            }
+        }
+    }
+
+    private class DownloadImageTaskSafe extends AsyncTask<String, String, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            imageProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            Bitmap bitmap = null;
+            try {
+                String path = Environment.getExternalStorageDirectory() + "/image.bmp";
+                URL url = new URL("http://guminegor.github.io/DownloadingPicture/images/image0.bmp");
+
+                URLConnection connection = url.openConnection();
+                File fileThatExists = new File(path);
+                OutputStream output = wasStoppedOnFile == -1 ? new FileOutputStream(path) :new FileOutputStream(path, true);
+                long downloadFrom = wasStoppedOnFile == -1 ? 0 : fileThatExists.length();
+                connection.setRequestProperty("Range", "bytes=" + downloadFrom + "-");
+
+                connection.connect();
+
+                int lenghtOfFile = connection.getContentLength();
+
+                InputStream input = new BufferedInputStream(connection.getInputStream());
+                byte data[] = new byte[1024];
+
+                long total = 0;
+                int count = 0;
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    publishProgress("" + (total + downloadFrom)* 100 / lenghtOfFile);
+                    output.write(data, 0 , count);
+                }
+
+
+
+
+
+                } catch (MalformedURLException e1) {
+                e1.printStackTrace();
+            } catch (FileNotFoundException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return null;
+        }
+
+
+
+        protected void onProgressUpdate(String... progress) {
+            imageProgressBar.setProgress(Integer.parseInt(progress[0]));
+        }
+
+        protected void onPostExecute(Bitmap bm) {
+
+            imageProgressBar.setVisibility(View.GONE);
+
+            if (bm != null) {
+                mImageView.setImageBitmap(bm);
+            }
+        }
     }
 
 }
